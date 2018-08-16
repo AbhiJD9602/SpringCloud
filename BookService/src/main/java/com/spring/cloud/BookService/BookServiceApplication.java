cac2f852428c5cef604b5515028da405e3dd596a
Abhinav <abhinav.soni13@gmail.com>
2018-08-17 00:08:06 +0530

    added hystrix

--- BookService/src/main/java/com/spring/cloud/BookService/BookServiceApplication.java
+++ BookService/src/main/java/com/spring/cloud/BookService/BookServiceApplication.java
@@ -4,6 +4,7 @@ import com.spring.cloud.BookService.pojo.Book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
--- RatingService/src/main/java/com/spring/cloud/RatingService/RatingServiceApplication.java
+++ RatingService/src/main/java/com/spring/cloud/RatingService/RatingServiceApplication.java
@@ -1,9 +1,14 @@
 package com.spring.cloud.RatingService;
 
+import com.spring.cloud.RatingService.pojo.Book;
 import com.spring.cloud.RatingService.pojo.Rating;
+import com.spring.cloud.RatingService.service.BookServiceDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,28 +21,38 @@ import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 @RestController
 @RequestMapping("/ratings")
 public class RatingServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RatingServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RatingServiceApplication.class, args);
+    }
 
-	private List<Rating> ratingList = Arrays.asList(
-			new Rating(1L, 1L, 2),
-			new Rating(2L, 1L, 3),
-			new Rating(3L, 2L, 4),
-			new Rating(4L, 2L, 5)
-	);
+    @Autowired
+    private BookServiceDelegate bookServiceDelegate;
 
-	@GetMapping("")
-	public List<Rating> findRatingsByBookId(@RequestParam Long bookId) {
-		return bookId == null || bookId.equals(0L) ? Collections.EMPTY_LIST : ratingList.stream().filter(r -> r.getBookId().equals(bookId)).collect(Collectors.toList());
-	}
+    private List<Rating> ratingList = Arrays.asList(
+            new Rating(1L, 1L, 2),
+            new Rating(2L, 1L, 3),
+            new Rating(3L, 2L, 4),
+            new Rating(4L, 2L, 5)
+    );
 
-	@GetMapping("/all")
-	public List<Rating> findAllRatings() {
-		return ratingList;
-	}
+    @GetMapping("")
+    public List<Rating> findRatingsByBookId(@RequestParam Long bookId) {
+        return bookId == null || bookId.equals(0L) ? Collections.EMPTY_LIST : ratingList.stream().filter(r -> r.getBookId().equals(bookId)).collect(Collectors.toList());
+    }
+
+    @GetMapping("/all")
+    public List<Rating> findAllRatings() {
+        return ratingList;
+    }
+
+    @GetMapping("/contactBookService")
+    public List<Book> contactBookService() {
+        return bookServiceDelegate.callBookServiceAndGetData();
+    }
 }
